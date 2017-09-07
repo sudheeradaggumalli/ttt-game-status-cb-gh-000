270295247ec82a849bd6a09e6ec423a7e5398a27
Sudheera Daggumalli <sudheeradaggumalli@gmail.com>
2017-09-07 07:48:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,7 +6,7 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS=[["0","1","2"],["3","4","5"],["6","7","8"],["0","3","6"],["1","4","7"],["2","5","8"],["0","4","8"],["2","4","3"]]
 
-def won(board)
+def won?(board)
   for each win in WIN_COMBINATIONS
         win_pos1=win[0]
         win_pos2=win[1]
@@ -23,5 +23,8 @@ def won(board)
   end
 end
 
-def full(board)
+def full?(board)
   board.all{|value| if value=="X" || value=="O"}
+end
+
+
