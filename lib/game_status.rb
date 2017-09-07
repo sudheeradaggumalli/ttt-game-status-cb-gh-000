cab9da1a9dbc2a4045d12a8dc465616ac71817e8
Sudheera Daggumalli <sudheeradaggumalli@gmail.com>
2017-09-07 08:12:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,7 @@ end
 WIN_COMBINATIONS=[["0","1","2"],["3","4","5"],["6","7","8"],["0","3","6"],["1","4","7"],["2","5","8"],["0","4","8"],["2","4","3"]]
 
 def won?(board)
-  for each win in WIN_COMBINATIONS
+  WIN_COMBINATIONS.each do |win|
         win_pos1=win[0]
         win_pos2=win[1]
         win_pos3=win[2]
